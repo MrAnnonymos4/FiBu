e4bee09f39a490c2d65c3f41401790ab1fd9610e
Halme96 <halme@dhbw-loerrach.de>
2018-04-09 17:35:57 +0200

    changed directories

--- FiBuApp/js/dragAndDrop.js
+++ FiBuApp/js/dragAndDrop.js
@@ -1,13 +1,26 @@
-function initialize() {
-    var offset_data; 
-    var dragme = document.getElementById("dragme");
-    console.log(dragme);
-    dragme.addEventListener('dragstart', drag_start, false);
+var lastClickedAsideId;
+
+document.addEventListener("DOMContentLoaded", anEvent => {
     document.body.addEventListener('dragover', drag_over, false);
     document.body.addEventListener('drop', drop, false);
+});
+
+function updateLastClickedAsideId(asideId) {
+    lastClickedAsideId = asideId;
 }
 
-function drag_start(event) {
+function initialize(asideId) {
+    var offset_data; 
+    //var dragme = document.getElementById("dragme");
+    var dragme = document.getElementById(asideId);
+    dragme.addEventListener('dragstart', function (anEvent) { drag_start(dragme, anEvent) });
+    
+    updateLastClickedAsideId(asideId);
+}
+
+function drag_start(aComponent, event) {
+    console.log(event);
+    updateLastClickedAsideId(aComponent.id);
     var style = window.getComputedStyle(event.target, null);
     offset_data = (parseInt(style.getPropertyValue("left"), 10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"), 10) - event.clientY);
     event.dataTransfer.setData("text/plain", offset_data);
@@ -20,7 +33,7 @@ function drag_over(event) {
     catch (e) {
         offset = offset_data.split(',');
     }
-    var dragme = document.getElementById('dragme');
+    var dragme = document.getElementById(lastClickedAsideId);
     dragme.style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
     dragme.style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
     event.preventDefault();
@@ -34,7 +47,7 @@ function drop(event) {
     catch (e) {
         offset = offset_data.split(',');
     }
-    var dragme = document.getElementById('dragme');
+    var dragme = document.getElementById(lastClickedAsideId);
     dragme.style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
     dragme.style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
     event.preventDefault();
